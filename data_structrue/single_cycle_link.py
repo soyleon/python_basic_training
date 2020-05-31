7cc09d31debd48ca8b9ffddbad802d077c922e51
soyleon <fuqiuhao@sina.com>
2020-05-31 19:50:53 -0300

    Operations of SingleCycleLink

--- data_structrue/single_cycle_link.py
+++ data_structrue/single_cycle_link.py
@@ -6,10 +6,12 @@ class Node(object):
         pass
 
 
-class SingleLinkList(object):
+class SingleCycleLinkList(object):
     """单链表"""
     def __init__(self, node=None):
         self.__head = node
+        if node:
+            node.next = node
 
     def is_empty(self):
         """判断链表是否为空"""
@@ -18,35 +20,51 @@ class SingleLinkList(object):
     def length(self):
         """返回链表长度"""
         cur = self.__head
-        count = 0
-        while cur != None:
-            count += 1
-            cur = cur.next
+        if cur == None:
+            count = 0
+        else:
+            count = 1
+            while cur.next != self.__head:  # 对于单向循环链表只能存在一种判别
+                count += 1
+                cur = cur.next
         return count
 
     def travel(self):
         """遍历整个链表"""
         cur = self.__head
-        while cur != None:
-            print(cur.elem, end=" ")
-            cur = cur.next
-        print()
+        if cur == None:
+            print("None")
+        else:
+            while cur.next != self.__head:
+                print(cur.elem, end=" ")
+                cur = cur.next
+            print(cur.elem)
 
     def add(self, item):
         """链表头部增加元素，头插法"""
+        cur = self.__head
         node = Node(item)
-        node.next = self.__head
-        self.__head = node
+        if cur == None:
+            self.__head = node
+            node.next = node
+        else:
+            while cur.next != self.__head:
+                cur = cur.next
+            node.next = self.__head
+            self.__head = node
+            cur.next = node
 
     def append(self, item):
         """链表尾部增加元素,尾插法"""
         node = Node(item)
         if self.is_empty():
             self.__head = node
+            node.next = node
         else:
             cur = self.__head
-            while cur.next != None:
+            while cur.next != self.__head:
                 cur = cur.next
+            node.next = self.__head
             cur.next = node
 
     def insert(self, pos, item):
@@ -70,13 +88,20 @@ class SingleLinkList(object):
     def remove(self, item):
         """删除节点"""
         if self.is_empty():
-            return self
+            return None
         else:
             pre = self.__head
             if pre.elem == item:
-                self.__head = pre.next
+                if pre.next == self.__head:
+                    self.__head = None
+                else:
+                    rear = self.__head
+                    while rear.next != self.__head:
+                        rear = rear.next
+                    self.__head = pre.next
+                    rear.next = self.__head
             else:
-                while pre.next != None :
+                while pre.next != self.__head:
                     if pre.next.elem == item:
                         pre.next = pre.next.next
                     else:
@@ -84,16 +109,29 @@ class SingleLinkList(object):
 
     def search(self, item):
         """查找结点是否存在"""
+        if self.is_empty():
+            return False
         cur = self.__head
-        while (cur != None):
+        while cur.next != self.__head:
             if cur.elem == item:
                 return True
-            cur = cur.next
+            else:
+                cur = cur.next
+        if cur.elem == item:
+            return True
         return False
 
 
+
 if __name__ == "__main__":
-    ll = SingleLinkList()
+
+    ll = SingleCycleLinkList()
+    ll.add(1)
+    ll.add(2)
+    ll.travel()
+    ll.remove(2)
+    ll.travel()
+
     print(ll.search(1))
     print(ll.is_empty())
     print(ll.length())
