9eeb595b845a2ff589f6dd2e001c9669f9bd47d1
soyleon <fuqiuhao@sina.com>
2020-06-08 21:46:48 -0300

    binary_search normal

--- data_structrue/binary_search.py
+++ data_structrue/binary_search.py
@@ -17,8 +17,23 @@ def binary_search(alist, item):
         return False
 
 
+def binary_search_normal(alist, item):
+    n = len(alist)
+    first = 0
+    last = n - 1
+    while first < last:
+        mid = (first + last) // 2
+        if alist[mid] == item:
+            return True
+        elif alist[mid] < item:
+            first = mid + 1
+        else:
+            last = mid - 1
+    return False
+
+
 if __name__ == "__main__":
     a_list = [12, 34, 32, 656, 65, 61, 78, 23, 6, 8, 4]
     quick_sort(a_list, 0, len(a_list)-1)
-    print(binary_search(a_list, 12))
-    print((binary_search(a_list, 8)))
+    print(binary_search_normal(a_list, 12))
+    print((binary_search_normal(a_list, 8)))
