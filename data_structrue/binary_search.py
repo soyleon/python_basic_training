449885d77ac8c2ae8843916181b0c707b11ecd8b
soyleon <fuqiuhao@sina.com>
2020-06-08 21:37:24 -0300

    binary_search recursion

---
+++ data_structrue/binary_search.py
@@ -0,0 +1,24 @@
+from quick_sort import quick_sort
+#  coding:utf-8
+
+
+def binary_search(alist, item):
+    """二分查找,递归版本"""
+    n = len(alist)
+    if n > 0:
+        mid = n // 2
+        if alist[mid] == item:
+            return True
+        elif alist[mid] < item:
+            return binary_search(alist[mid+1:], item)
+        else:
+            return binary_search(alist[:mid], item)
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    a_list = [12, 34, 32, 656, 65, 61, 78, 23, 6, 8, 4]
+    quick_sort(a_list, 0, len(a_list)-1)
+    print(binary_search(a_list, 12))
+    print((binary_search(a_list, 8)))
